446ff5868cc0496df07f774241e1d2d473596548
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-09 14:17:49 +0200

    feat: add login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 
-const Login = () => (
-    <div>
-        Login Component
-    </div>
-);
+const Login = () => {
+    const [data, setData] = useState({email: "", password: ""})
+    const [error, setError] = useState("")
+
+    const handleChange = ({currentTarget: input}) => {
+        setData({...data, [input.name]: input.value})
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const url = "http://localhost:5000/api/auth"
+            const {data: res} = await axios.post(url, data)
+            localStorage.setItem("token", res.data)
+            window.location = "/"
+        } catch (error) {
+            if (
+                error.response &&
+                error.response.status >= 400 &&
+                error.response.status <= 500
+            ) {
+                setError(error.response.data.message)
+            }
+        }
+    }
+
+    return (
+        <div className="col-11 col-sm-8 col-md-6 col-lg-4 col-xl-3 mx-auto my-auto bg-light rounded-3 p-5 shadow">
+            {error && <div className="alert alert-danger text-center">{error}</div>}
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label className="form-label">Email address</label>
+                    <input type="email" className="form-control" required
+                           onChange={handleChange} value={data.email}/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Password</label>
+                    <input type="password" className="form-control" required
+                           onChange={handleChange} value={data.password}/>
+                </div>
+                <div className="d-grid">
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </div>
+            </form>
+        </div>
+    );
+};
 
 Login.propTypes = {};
 
