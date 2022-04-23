c38f7c7f401166a24567097243e09f6a89b0926d
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-24 00:36:25 +0200

    fix: use floating labels in login and registration input fields

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,5 +1,5 @@
 import React, {FC, useContext, useState} from 'react';
-import {Alert, Button, Col, Form, InputGroup} from "react-bootstrap";
+import {Alert, Button, Col, FloatingLabel, Form} from "react-bootstrap";
 import {AxiosError, AxiosResponse} from "axios";
 import {useNavigate} from "react-router-dom";
 import {TokenContext} from "../../App";
@@ -47,39 +47,32 @@ const Login: FC<LoginProps> = ({redirectTo}) => {
         <Col xs={11} sm={8} md={6} lg={5} xl={4} xxl={3} className="mx-auto my-auto bg-light rounded-3 p-5 shadow">
             {serverError && <Alert variant="danger" className="text-center">{serverError}</Alert>}
             <Form onSubmit={formik.handleSubmit} noValidate>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="inputEmail">Email address</Form.Label>
-                    <InputGroup hasValidation>
-                        <Form.Control
-                            type="email"
-                            name="email"
-                            id="inputEmail"
-                            onChange={formik.handleChange}
-                            value={formik.values.email}
-                            isValid={formik.touched.email && !formik.errors.email}
-                            isInvalid={formik.touched.email && !!formik.errors.email}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {formik.errors.email}
-                        </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
-                <Form.Group className="mb-4">
-                    <Form.Label htmlFor="inputPassword">Password</Form.Label>
-                    <InputGroup hasValidation>
-                        <Form.Control
-                            type="password"
-                            name="password"
-                            id="inputPassword"
-                            onChange={formik.handleChange}
-                            value={formik.values.password}
-                            isInvalid={formik.touched.password && !!formik.errors.password}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {formik.errors.password}
-                        </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
+                <FloatingLabel controlId="inputEmail" label="Email address" className="mb-3">
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        id="inputEmail"
+                        placeholder="name@domain.com"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        isValid={formik.touched.email && !formik.errors.email}
+                        isInvalid={formik.touched.email && !!formik.errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">{formik.errors.email}</Form.Control.Feedback>
+                </FloatingLabel>
+                <FloatingLabel controlId="inputPassword" label="Password" className="mb-4">
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        id="inputPassword"
+                        placeholder="Password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        isValid={formik.touched.password && !formik.errors.password}
+                        isInvalid={formik.touched.password && !!formik.errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">{formik.errors.password}</Form.Control.Feedback>
+                </FloatingLabel>
                 <Form.Group className="d-grid">
                     <Button type="submit" variant="primary">Login</Button>
                 </Form.Group>
--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,6 +1,6 @@
 import React, {FC, useState} from 'react';
 import {useNavigate} from "react-router-dom";
-import {Alert, Button, Col, Form, InputGroup} from "react-bootstrap";
+import {Alert, Button, Col, FloatingLabel, Form} from "react-bootstrap";
 import useAxios from "../../hooks/useAxios";
 import * as yup from "yup";
 import YupPassword from 'yup-password'
@@ -51,90 +51,78 @@ const Register: FC<RegisterProps> = () => {
         <Col xs={11} sm={8} md={6} lg={5} xl={4} xxl={3} className="mx-auto my-auto bg-light rounded-3 p-5 shadow">
             {serverError && <Alert variant="danger" className="text-center">{serverError}</Alert>}
             <Form onSubmit={formik.handleSubmit} noValidate>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="inputEmail">Email address</Form.Label>
-                    <InputGroup hasValidation>
-                        <Form.Control
-                            type="email"
-                            name="email"
-                            id="inputEmail"
-                            onChange={formik.handleChange}
-                            value={formik.values.email}
-                            isValid={formik.touched.email && !formik.errors.email}
-                            isInvalid={formik.touched.email && !!formik.errors.email}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {formik.errors.email}
-                        </Form.Control.Feedback>
-                    </InputGroup>
+                <FloatingLabel controlId="inputEmail" label="Email address" className="mb-3">
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        id="inputEmail"
+                        placeholder="name@domain.com"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        isValid={formik.touched.email && !formik.errors.email}
+                        isInvalid={formik.touched.email && !!formik.errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">{formik.errors.email}</Form.Control.Feedback>
                     <Form.Text id="emailHelp" className="form-text">
                         We'll never share your email with anyone else.
                     </Form.Text>
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="inputPassword">Password</Form.Label>
-                    <InputGroup hasValidation>
-                        <Form.Control
-                            type="password"
-                            name="password"
-                            id="inputPassword"
-                            onChange={formik.handleChange}
-                            value={formik.values.password}
-                            isInvalid={formik.touched.password && !!formik.errors.password}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {formik.errors.password}
-                        </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="inputPasswordConfirmation">Password confirmation</Form.Label>
-                    <InputGroup hasValidation>
-                        <Form.Control
-                            type="password"
-                            name="passwordConfirmation"
-                            id="inputPasswordConfirmation"
-                            onChange={formik.handleChange}
-                            value={formik.values.passwordConfirmation}
-                            isInvalid={formik.touched.passwordConfirmation && !!formik.errors.passwordConfirmation}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {formik.errors.passwordConfirmation}
-                        </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="inputFirstName">First name</Form.Label>
-                    <InputGroup hasValidation>
-                        <Form.Control
-                            type="text"
-                            name="firstName"
-                            id="inputFirstName"
-                            onChange={formik.handleChange}
-                            value={formik.values.firstName}
-                            isInvalid={formik.touched.firstName && !!formik.errors.firstName}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {formik.errors.firstName}
-                        </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
-                <Form.Group className="mb-4">
-                    <Form.Label htmlFor="inputLastName">Last name</Form.Label>
-                    <InputGroup hasValidation>
-                        <Form.Control
-                            type="text"
-                            name="lastName"
-                            id="inputLastName"
-                            onChange={formik.handleChange}
-                            value={formik.values.lastName}
-                            isInvalid={formik.touched.lastName && !!formik.errors.lastName}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {formik.errors.lastName}
-                        </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
+                </FloatingLabel>
+                <FloatingLabel controlId="inputPassword" label="Password" className="mb-3">
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        id="inputPassword"
+                        placeholder="Password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        isValid={formik.touched.password && !formik.errors.password}
+                        isInvalid={formik.touched.password && !!formik.errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">{formik.errors.password}</Form.Control.Feedback>
+                </FloatingLabel>
+                <FloatingLabel controlId="inputPasswordConfirmation" label="Password confirmation" className="mb-3">
+                    <Form.Control
+                        type="password"
+                        name="passwordConfirmation"
+                        id="inputPasswordConfirmation"
+                        placeholder="Password confirmation"
+                        onChange={formik.handleChange}
+                        value={formik.values.passwordConfirmation}
+                        isValid={formik.touched.passwordConfirmation && !formik.errors.passwordConfirmation}
+                        isInvalid={formik.touched.passwordConfirmation && !!formik.errors.passwordConfirmation}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.passwordConfirmation}
+                    </Form.Control.Feedback>
+                </FloatingLabel>
+                <FloatingLabel controlId="inputFirstName" label="First name" className="mb-3">
+                    <Form.Control
+                        type="text"
+                        name="firstName"
+                        id="inputFirstName"
+                        placeholder="First name"
+                        onChange={formik.handleChange}
+                        value={formik.values.firstName}
+                        isInvalid={formik.touched.firstName && !!formik.errors.firstName}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.firstName}
+                    </Form.Control.Feedback>
+                </FloatingLabel>
+                <FloatingLabel controlId="inputLastName" label="Last name" className="mb-4">
+                    <Form.Control
+                        type="text"
+                        name="lastName"
+                        id="inputLastName"
+                        placeholder="Last name"
+                        onChange={formik.handleChange}
+                        value={formik.values.lastName}
+                        isInvalid={formik.touched.lastName && !!formik.errors.lastName}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.lastName}
+                    </Form.Control.Feedback>
+                </FloatingLabel>
                 <Form.Group className="d-grid">
                     <Button type="submit" variant="primary">Register</Button>
                 </Form.Group>
